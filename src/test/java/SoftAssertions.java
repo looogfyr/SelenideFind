09261296268408b6da8ea41dfa52c9739d9c150c
Alex Dubinin <alexdubinin88@gmail.com>
2025-03-25 15:15:34 +0900

    add code and rename

--- src/test/java/SoftAssertions.java
+++ src/test/java/SoftAssertions.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class SoftAssertionsForMyFriend {
+public class SoftAssertions{
 
     String snippetJUnit5 = """
                 @ExtendWith({SoftAssertsExtension.class})
@@ -23,19 +23,19 @@ public class SoftAssertionsForMyFriend {
                 """;
 
     @BeforeAll
-    static void beforeall() {
+    static void beforeAll() {
         Configuration.browserSize = "1450x700";
         Configuration.baseUrl = "https://github.com/";
         Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
-    void SoftAssertions() {
+    void softAssertions() {
         open("/selenide/selenide");
         $("#wiki-tab").click();
         $("#wiki-pages-filter").setValue("SoftAssertions");
-        $("span[data-view-component='true'] a.truncate-text").shouldHave(text("SoftAssertions"));
-        $("span[data-view-component='true'] a.truncate-text").$(byText("SoftAssertions")).click();
+        $(".js-wiki-sidebar-toggle-display").shouldHave(text("SoftAssertions"));
+        $(".js-wiki-sidebar-toggle-display").$(byText("SoftAssertions")).click();
         $("#wiki-body").shouldHave(text(snippetJUnit5));
     }
 }
